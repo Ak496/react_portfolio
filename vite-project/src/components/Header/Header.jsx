dc905107495206038f36d92594cf88b7dd2d4455
akila.a <akila.a@infosys.com>
2024-08-11 02:38:01 +0530

    changes added in header nav bar

--- vite-project/src/components/Header/Header.jsx
+++ vite-project/src/components/Header/Header.jsx
@@ -2,8 +2,9 @@ import './Header.css'
 
 const Header = () => {
     return (
-        <div id="header-section">
-            <nav>
+        <div id="header-container">
+            <nav id="header-section">
+                <h3 id="header-section-heading">AKILA ARUNASATESAN</h3>
                 <ul id="nav-link-list">
                     <li><a id="nav-link-home" href="#home">HOME</a></li>
                     <li><a id="nav-link-about" href="#about">ABOUT ME</a></li>
