e7f73922a499e422fe867bd7797536591772de59
akila.a <akila.a@infosys.com>
2024-08-20 01:17:13 +0530

    footer added

--- vite-project/src/components/Footer/Footer.jsx
+++ vite-project/src/components/Footer/Footer.jsx
@@ -1,4 +1,12 @@
+import reactIcon from "../../assets/React.svg"
+import "./Footer.css"
 const Footer = () =>{
+    return(
+        <div id="footer">
+           <h4>This webiste is built using </h4>
+           <img src={reactIcon} width="20" height="20"/>
+        </div>
+    )
 
 }
 export default Footer;
