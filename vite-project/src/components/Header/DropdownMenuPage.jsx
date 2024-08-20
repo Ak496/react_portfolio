dbbc8651504ca9107ff15a37e4bd35f15a5a9d05
akila.a <akila.a@infosys.com>
2024-08-20 19:41:28 +0530

    Header style and media queries modified

--- vite-project/src/components/Header/DropdownMenuPage.jsx
+++ vite-project/src/components/Header/DropdownMenuPage.jsx
@@ -8,7 +8,7 @@ const DropdownMenuPage = ({ onClickHandler }) => {
                 <li><a href="#home">Home</a></li>
                 <li><a href="#about">About</a></li>
                 <li><a href="#contact">Contact</a></li>
-                <li><button id="resume-button">RESUME</button></li>
+                <li><button id="menu-resume-button">RESUME</button></li>
             </ul>
         </div>
     )
