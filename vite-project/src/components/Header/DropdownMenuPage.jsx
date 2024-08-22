385165a754f2e384a51d9f09f7b2c210fdf042c8
akila.a <akila.a@infosys.com>
2024-08-22 14:22:37 +0530

    Spelling corrected in Dropdown menu

--- vite-project/src/components/Header/DropdownMenuPage.jsx
+++ vite-project/src/components/Header/DropdownMenuPage.jsx
@@ -5,7 +5,7 @@ const DropdownMenuPage = () => {
             <ul id="menu-dropDown">
                 <li><a href="#home">HOME</a></li>
                 <li><a href="#about">ABOUT ME</a></li>
-                <li><a href="#skill">SKILL</a></li>
+                <li><a href="#skill">SKILLS</a></li>
                 <li><a href="#contact">CONTACT</a></li>
                 <li><button id="menu-resume-button">RESUME</button></li>
             </ul>
