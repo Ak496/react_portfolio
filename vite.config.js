1c86eb822492a052737e14423d87d90477aa0abb
akila.a <akila.a@infosys.com>
2024-08-23 03:07:56 +0530

    Vite configuration updated with babel

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { createBabelPlugin } from 'vite-plugin-babel'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createBabelPlugin({
+      babelConfig: {
+        presets: ["@babel/preset-env", "@babel/preset-react"]
+      }
+    })
+  ],
   resolve: {
     preserveSymlinks: true
   }
