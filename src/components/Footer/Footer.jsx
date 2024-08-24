ca6a30046672a3ffc46cb1230d27887f0b578218
akila.a <akila.a@infosys.com>
2024-08-24 16:26:53 +0530

    CSS adjusted

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,7 @@ import "./Footer.css"
 const Footer = () => {
     return (
         <div id="footer">
-            <p>&copy; 2024</p>
-            <div id="footer-content">
+            <div id="footer-content1">
                 <p>This website was made with </p> &nbsp;
                 <img src={reactIcon} width="20" height="20" />
             </div>
@@ -17,19 +16,20 @@ const Footer = () => {
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <img src={linkedin} alt="Linkedin" width="40" height="40" />
+                    <img src={linkedin} alt="Linkedin" width="30" height="30" />
                 </a>
                 <a
                     href="https://github.com/Ak496/"
                     target="_blank"
                     rel="noreferrer"
                 >
-                    <img src={githubIcon} alt="GitHub" width="40" height="40" />
+                    <img src={githubIcon} alt="GitHub" width="30" height="25" />
                 </a>
                 <a href="mailto:akilaa.arun24@gmail.com">
-                    <img src={gmailIcon} alt="Email"  width="40" height="40" />
+                    <img src={gmailIcon} alt="Email"  width="30" height="30" />
                 </a> 
             </div>
+            <p id="footer-content2">&copy;copyright. All rights reserved.</p>
         </div>
     )
 }
